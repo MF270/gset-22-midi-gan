c5200ff94527c9a55fb37b865c8c6308b5beea9d
k r <rck6789@gmail.com>
2022-07-13 15:47:39 -0400

    gan :(

--- gan3.py
+++ gan3.py
@@ -7,17 +7,22 @@ from torch.utils.tensorboard import SummaryWriter
 class Discriminator(nn.Module):
 	def __init__(self, in_features):
 		super().__init__()
-		self.disc = nn.Sequential(
-			nn.Linear(int_features, 128),
-			nn.LeakyReLU(0.1),
-			nn.Linear(128, 1),
-			nn.Sigmoid(),
-		)
+		self.fc1 = nn.Linear(1280, 600)
+		self.fc2 = nn.Linear(600, 300)
+		self.fc3 = nn.Linear(300, 100)
+		self.fc4 = nn.Linear(100, 20)
+		self.fc5 = nn.Linear(20, 1)
 
 	def forward(self, x):
-		retirn self.disc(x)
+		x = nn.LeakyReLU(self.fc1(x), 0.2)
+		x = nn.LeakyReLU(self.fc2(x), 0.2)
+		x = nn.LeakyReLU(self.fc3(x), 0.2)
+		x = nn.LeakyReLU(self.fc4(x), 0.2)
+		x = nn.LeakyReLU(self.fc5(x), 0.2)
+		x = nn.Sigmoid()
+		return self.disc(x)
 
-#midi dimension = 1280
+#midi dimension = 1281
 
 class Generator(nn.Module):
 	def __init__(self, z_dim, m_dim):
@@ -40,10 +45,10 @@ midi_dim = 256 * 5
 batch_size = 32
 num_epochs = 50
 
-disc = Discrmininator(midi_dim).to(device)
+disc = Discriminator(midi_dim).to(device)
 gen = Generator(z_dim, midi_dim).to(device)
 fixed_noise = torch.randn((batch_size, z_dim)).to(device)
-transforms =
+
 
 
 opt+disc = optim.Adam(disc.parameters(), lr=lr)
