afb9780d8d7d3b9e1b3e3338fd4ba1cbc8b5f85d
k r <rck6789@gmail.com>
2022-07-13 23:28:27 -0400

    gan :(

--- gan3.py
+++ gan3.py
@@ -16,10 +16,15 @@ class Discriminator(nn.Module):
 
 	def forward(self, x):
 		x = F.LeakyReLU(self.fc1(x), 0.2)
+		x = F.dropout(x, 0.3)
 		x = F.LeakyReLU(self.fc2(x), 0.2)
+		x = F.dropout(x, 0.3)
 		x = F.LeakyReLU(self.fc3(x), 0.2)
+		x = F.dropout(x, 0.3)
 		x = F.LeakyReLU(self.fc4(x), 0.2)
+		x = F.dropout(x, 0.3)
 		x = F.LeakyReLU(self.fc5(x), 0.2)
+		x = F.dropout(x, 0.3)
 		x = nn.Sigmoid()
 		return self.disc(x)
 
@@ -59,7 +64,8 @@ disc = Discriminator(midi_dim).to(device)
 gen = Generator(z_dim, midi_dim).to(device)
 fixed_noise = torch.randn((batch_size, z_dim)).to(device)
 
-
+#build network
+#WTF!!!! ? ? 
 
 opt+disc = optim.Adam(disc.parameters(), lr=lr)
 opt_gen = optim.Adam(gen.parameters(), lr=lr)
@@ -68,6 +74,37 @@ writer_fake = SummmaryWriter(f"runs/GAN_MIDI/fake")
 writer_real = SummaryWriter(f"runs/GAN_MIDI/real")
 step = 0
 
-for epoch in range(num_epochs):
-	for batch_idx, (real, _) in enumerate(loader):
-		real = real.
+def D_train(x):
+	D.zero_grad()
+
+	#get the real data to do the yeah 
+
+	D_output = D(x_real)
+	D_real_loss = criterion(D_output, y_real) #y_real should just be all 1s
+	D_real_score = D_output
+
+	#get the fake data to do the yeah
+
+	D_output = D(x_fake)
+	D_fake_loss = criterion(D_output, y_fake) #y_fake should just be all 0s
+	D_fake_score = D_output
+
+	D_loss = D_real_loss + D_fake_loss
+	D_loss.backward()
+	D_optimizer.step()
+
+	return D_Loss.data.item()
+
+def G_train(x):
+	G.zero_grad()
+
+	#idk how u get the stuff
+
+	G_output = G(z)
+	D_output = D(G_output)
+	G_loss = criterion(D_output, y)
+
+	G_loss.backward()
+	G_optimizer.step()
+
+	return G_loss.data.item()
