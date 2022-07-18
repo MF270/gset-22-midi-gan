c28e336fa685189ece2ee4a4d519bd21db3b16ea
k r <rck6789@gmail.com>
2022-07-18 14:19:54 -0400

    bruv

--- bidirectional_rnn_tf.py
+++ bidirectional_rnn_tf.py
@@ -22,6 +22,8 @@ import os
 import io
 import time
 
+DIR_TO_CSVS = r"C:\Users\rck67\GSET\rnn_csv"
+
 def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
 
     notes, frames = piano_roll.shape
@@ -53,6 +55,144 @@ def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
     pm.instruments.append(instrument)
     return pm
 
+def list_midi(folder='maestro-v1.0.0/**/*.midi',seed_int=123):
+	list_all_midi = glob.glob(folder)
+	seed(seed_int)
+	shuffle(list_all_midi)
+	return list_all_midi
+
+list_all_midi = get_list_midi()
+
+class NoteTokenizer:
+    
+    def __init__(self):
+    	self.notes_to_index = {}
+    	self.index_to_notes = {}
+    	self.num_of_word = 0
+    	self.unique_word = 0
+		self.notes_freq = {}
+		
+    def transform(self, list_array):
+    	transformed_list = []
+     	for instance in list_array:
+        transformed_list.append([self.notes_to_index[note] for note in instance])
+      	return np.array(transformed_list, dtype=np.int32)
+
+    def partial_fit(self, notes):
+       for note in notes:
+            note_str = ','.join(str(a) for a in note)
+            if note_str in self.notes_freq:
+                self.notes_freq[note_str] += 1
+                self.num_of_word += 1
+            else:
+                self.notes_freq[note_str] = 1
+                self.unique_word += 1
+                self.num_of_word += 1
+                self.notes_to_index[note_str], self.index_to_notes[self.unique_word] = self.unique_word, note_str
+
+    def add_new_note(self, note):
+        assert note not in self.notes_to_index
+        self.unique_word += 1
+        self.notes_to_index[note], self.index_to_notes[self.unique_word] = self.unique_word, note
+
+    def add_new_note(self, note):
+    	assert note not in self.notes_to_index
+        self.unique_word += 1
+        self.notes_to_index[note], self.index_to_notes[self.unique_word] = self.unique_word, note
+
+def generate_batch_song(list_all_midi, batch_music=16, start_index=0, fs=30, seq_len=50, use_tqdm=False):
+	assert len(list_all_midi) >= batch_music
+    dict_time_notes = generate_dict_time_notes(list_all_midi, batch_music, start_index, fs, use_tqdm=use_tqdm)
+    
+    list_musics = process_notes_in_song(dict_time_notes, seq_len)
+    collected_list_input, collected_list_target = [], []
+     
+    for music in list_musics:
+        list_training, list_target = generate_input_and_target(music, seq_len)
+        collected_list_input += list_training
+        collected_list_target += list_target
+    return collected_list_input, collected_list_target
+
+def generate_dict_time_notes(list_all_midi, batch_song = 16, start_index=0, fs=30, use_tqdm=True):
+	assert len(list_all_midi) >= batch_song
+    
+    dict_time_notes = {}
+    process_tqdm_midi = tqdm_notebook(range(start_index, min(start_index + batch_song, len(list_all_midi)))) if use_tqdm else range(start_index,  min(start_index + batch_song, len(list_all_midi)))
+    for i in process_tqdm_midi:
+        midi_file_name = list_all_midi[i]
+        if use_tqdm:
+            process_tqdm_midi.set_description("Processing {}".format(midi_file_name))
+        try: # Handle exception on malformat MIDI files
+            midi_pretty_format = pretty_midi.PrettyMIDI(midi_file_name)
+            piano_midi = midi_pretty_format.instruments[0] # Get the piano channels
+            piano_roll = piano_midi.get_piano_roll(fs=fs)
+            dict_time_notes[i] = piano_roll
+        except Exception as e:
+            print(e)
+            print("broken file : {}".format(midi_file_name))
+            pass
+    return dict_time_notes
+
+def generate_input_and_target(dict_keys_time, seq_len=50):
+	start_time, end_time = list(dict_keys_time.keys())[0], list(dict_keys_time.keys())[-1]
+    list_training, list_target = [], []
+    for index_enum, time in enumerate(range(start_time, end_time)):
+        list_append_training, list_append_target = [], []
+        start_iterate = 0
+        flag_target_append = False # flag to append the test list
+        if index_enum < seq_len:
+            start_iterate = seq_len - index_enum - 1
+            for i in range(start_iterate): # add 'e' to the seq list. 
+                list_append_training.append('e')
+                flag_target_append = True
+
+        for i in range(start_iterate,seq_len):
+            index_enum = time - (seq_len - i - 1)
+            if index_enum in dict_keys_time:
+                list_append_training.append(','.join(str(x) for x in dict_keys_time[index_enum]))      
+            else:
+                list_append_training.append('e')
+
+        # add time + 1 to the list_append_target
+        if time+1 in dict_keys_time:
+            list_append_target.append(','.join(str(x) for x in dict_keys_time[time+1]))
+        else:
+            list_append_target.append('e')
+        list_training.append(list_append_training)
+        list_target.append(list_append_target)
+    return list_training, list_target
+
+def process_notes_in_song(dict_time_notes, seq_len = 50):
+	list_of_dict_keys_time = []
+    
+    for key in dict_time_notes:
+        sample = dict_time_notes[key]
+        times = np.unique(np.where(sample > 0)[1])
+        index = np.where(sample > 0)
+        dict_keys_time = {}
+
+        for time in times:
+            index_where = np.where(index[1] == time)
+            notes = index[0][index_where]
+            dict_keys_time[time] = notes
+        list_of_dict_keys_time.append(dict_keys_time)
+    return list_of_dict_keys_time
+
+sampled_200_midi = list_all_midi[0:100]  
+
+batch = 1
+start_index = 0
+note_tokenizer = NoteTokenizer()
+
+for i in tqdm_notebook(range(len(sampled_200_midi))):
+    dict_time_notes = generate_dict_time_notes(sampled_200_midi, batch_song=1, start_index=i, use_tqdm=False, fs=5)
+    full_notes = process_notes_in_song(dict_time_notes)
+    for note in full_notes:
+        note_tokenizer.partial_fit(list(note.values()))
+
+note_tokenizer.add_new_note('e')
+unique_notes = note_tokenizer.unique_word
+
 def create_model(seq_len, unique_notes, dropout=0.2, output_emb=100, rnn_u=128, dense_u=64):
   inputs = tf.keras.layers.Input(shape=(seq_len,))
   embedding = tf.keras.layers.Embedding(input_dim=unique_notes+1, output_dim=output_emb, input_length=seq_len)(inputs)
@@ -60,7 +200,7 @@ def create_model(seq_len, unique_notes, dropout=0.2, output_emb=100, rnn_u=128, 
   fp = tf.keras.layers.Dropout(dropout)(fp)
   fp = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(rnn_u, return_sequences=True))(fp)
   fp = tf.keras.layers.Dropout(dropout)(fp)
-  fp = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(rnn_u))(fp)
+  fp = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(rnn_u))(fp) #not sure why not returning sequences here but yea
   fp = tf.keras.layers.Dropout(dropout)(fp)
   fp = tf.keras.layers.Dense(dense_u)(fp)
   fp = tf.keras.layers.LeakyReLU()(fp)
@@ -70,3 +210,90 @@ def create_model(seq_len, unique_notes, dropout=0.2, output_emb=100, rnn_u=128, 
   return model
 
 model = create_model(seq_len, unique_notes)
+model.summary()
+
+import os
+optimizer = Nadam()
+
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                 model=model)
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+loss_fn = sparse_categorical_crossentropy
+
+class TrainModel:
+  
+  def __init__(self, epochs, note_tokenizer, sampled_200_midi, frame_per_second, 
+               batch_nnet_size, batch_song, optimizer, checkpoint, loss_fn,
+               checkpoint_prefix, total_songs, model):
+    self.epochs = epochs
+    self.note_tokenizer = note_tokenizer
+    self.sampled_200_midi = sampled_200_midi
+    self.frame_per_second = frame_per_second
+    self.batch_nnet_size = batch_nnet_size
+    self.batch_song = batch_song
+    self.optimizer = optimizer
+    self.checkpoint = checkpoint
+    self.loss_fn = loss_fn
+    self.checkpoint_prefix = checkpoint_prefix
+    self.total_songs = total_songs
+    self.model = model
+    
+  def train(self):
+    for epoch in tqdm_notebook(range(self.epochs),desc='epochs'):
+      # for each epochs, we shufle the list of all the datasets
+      shuffle(self.sampled_200_midi)
+      loss_total = 0
+      steps = 0
+      steps_nnet = 0
+
+      # We will iterate all songs by self.song_size
+      for i in tqdm_notebook(range(0,self.total_songs, self.batch_song), desc='MUSIC'):
+
+        steps += 1
+        inputs_nnet_large, outputs_nnet_large = generate_batch_song(
+            self.sampled_200_midi, self.batch_song, start_index=i, fs=self.frame_per_second, 
+            seq_len=seq_len, use_tqdm=False) # We use the function that have been defined here
+        inputs_nnet_large = np.array(self.note_tokenizer.transform(inputs_nnet_large), dtype=np.int32)
+        outputs_nnet_large = np.array(self.note_tokenizer.transform(outputs_nnet_large), dtype=np.int32)
+
+        index_shuffled = np.arange(start=0, stop=len(inputs_nnet_large))
+        np.random.shuffle(index_shuffled)
+
+        for nnet_steps in tqdm_notebook(range(0,len(index_shuffled),self.batch_nnet_size)):
+          steps_nnet += 1
+          current_index = index_shuffled[nnet_steps:nnet_steps+self.batch_nnet_size]
+          inputs_nnet, outputs_nnet = inputs_nnet_large[current_index], outputs_nnet_large[current_index]
+          
+          # To make sure no exception thrown by tensorflow on autograph
+          if len(inputs_nnet) // self.batch_nnet_size != 1:
+            break
+          loss = self.train_step(inputs_nnet, outputs_nnet)
+          loss_total += tf.math.reduce_sum(loss)
+          if steps_nnet % 20 == 0:
+            print("epochs {} | Steps {} | total loss : {}".format(epoch + 1, steps_nnet, loss_total))
+
+      checkpoint.save(file_prefix = self.checkpoint_prefix)
+  
+  @tf.function
+  def train_step(self, inputs, targets):
+    with tf.GradientTape() as tape:
+      prediction = self.model(inputs)
+      loss = self.loss_fn(targets, prediction)
+    gradients = tape.gradient(loss, self.model.trainable_variables)
+    self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+    return loss
+
+seq_len = 50
+EPOCHS = 4
+BATCH_SONG = 16
+BATCH_NNET_SIZE = 96
+TOTAL_SONGS = len(sampled_200_midi)
+FRAME_PER_SECOND = 5
+
+train_class = TrainModel(EPOCHS, note_tokenizer, sampled_200_midi, FRAME_PER_SECOND,
+                  BATCH_NNET_SIZE, BATCH_SONG, optimizer, checkpoint, loss_fn,
+                  checkpoint_prefix, TOTAL_SONGS, model)
+
+train_class.train()
+
